bcc590dba9297e52a74d8f7e6031b80fa41c8c47
Leon <lowpolygonum@gmail.com>
2019-07-21 17:41:16 +0200

    hop

--- browser/asset/shader/vegetal.glsl
+++ browser/asset/shader/vegetal.glsl
@@ -6,7 +6,6 @@ vec3 branchPosition (vec3 origin, vec3 seed, float ratio, float twistSin, float 
 	pos.yz *= rot(dist);
 	pos.xz *= rot(dist);
 	pos.xy *= rot(dist);
-	// pos.y -= 0.01/length(pos.xz) - 1.0;
 	pos += origin;
 	pos.x += (noise(noiseScale*(pos))*2.0-1.0)*noiseRange;
 	pos.y += (noise(noiseScale*(pos+vec3(2.3210,8.139,43.67)))*2.0-1.0)*noiseRange;
